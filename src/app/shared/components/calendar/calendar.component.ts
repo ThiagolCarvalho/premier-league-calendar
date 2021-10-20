7e9b5b904e38142066f2cbbd5871e98d49af7ad5
thiago <thiagol.carvalho97@gmail.com>
2021-10-20 01:03:09 -0300

    Added function that tracks how many days a month have.

--- src/app/shared/components/calendar/calendar.component.ts
+++ src/app/shared/components/calendar/calendar.component.ts
@@ -7,10 +7,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
+  public daysInMonth: number[];
+
   constructor() {
+    this.daysInMonth = [];
   }
 
   ngOnInit(): void {
+    const now = new Date()
+    this.setCountDaysInMonth(now.getFullYear(), now.getMonth() + 1);
+  }
+
+  setCountDaysInMonth(year: number, month: number) {
+    const countDaysInMonth = new Date(year, month, 0).getDate()
+    this.daysInMonth = new Array(countDaysInMonth).fill(0).map((x,i)=>i+1);
   }
 
 }
