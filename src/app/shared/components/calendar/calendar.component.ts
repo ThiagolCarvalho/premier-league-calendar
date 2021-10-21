956d04e884d3826aee6fc3403cf10219c1e55319
thiago <thiagol.carvalho97@gmail.com>
2021-10-21 00:59:38 -0300

    Added match events to the calendar, added grid column start

--- src/app/shared/components/calendar/calendar.component.ts
+++ src/app/shared/components/calendar/calendar.component.ts
@@ -1,40 +1,95 @@
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
+import {IPremierLeague} from "../../models/IPremierLeague";
+import {ICalendarDay} from "../../models/ICalendar";
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent implements OnInit, OnChanges {
-  @Input() data: any;
+export class CalendarComponent implements OnChanges {
+  @Input() data: IPremierLeague = {name: '', matches: []};
 
-  public daysInMonth: number[];
-  public monthName: string;
+  public daysInMonth: number[] = [];
+  public title: string = '';
   public daysOfTheWeek: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  public startDay: string = 'first-day';
+  public calendarDays: ICalendarDay[] = []
 
   constructor() {
-    this.daysInMonth = [];
-    this.monthName = '';
-  }
-
-  ngOnInit(): void {
-    const now = new Date()
-    this.setDaysInMonth(now.getFullYear(), now.getMonth() + 1);
-    this.setMonth(now);
   }
 
   ngOnChanges() {
-    console.log(this.data);
+    this.calendarDays = [];
+    this.loadCalendar();
   }
 
-  setDaysInMonth(year: number, month: number) {
+  private loadCalendar() {
+    const now = new Date(2019, 11, 22)
+    this.setDaysInMonth(now.getFullYear(), now.getMonth() + 1);
+    this.setCalendar(now);
+    this.setMonth(now);
+    this.setStartDay(now);
+  }
+
+  private setCalendar(date: Date) {
+    const countDaysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+    const days = new Array(countDaysInMonth).fill(0).map((x,i)=>(i+1).toString());
+    days.forEach(day => {
+      if (+day < 10) {
+        day = '0' + day;
+      }
+      const matches = this.data.matches.filter(match => match.date === `${date.getFullYear()}-${date.getMonth()+1}-${day}`)
+      this.calendarDays.push({day: day, matches: matches})
+    })
+    console.log(this.calendarDays);
+  }
+
+  private setDaysInMonth(year: number, month: number) {
     const countDaysInMonth = new Date(year, month, 0).getDate()
     this.daysInMonth = new Array(countDaysInMonth).fill(0).map((x,i)=>i+1);
-    console.log(this.daysInMonth);
   }
 
-  setMonth(date: Date) {
-    this.monthName = date.toLocaleString('en', { month: 'long' });
+  private setMonth(date: Date) {
+    this.title = date.getFullYear() + ' - ' + date.toLocaleString('en', { month: 'long' });
+  }
+
+  private setStartDay(date: Date) {
+    const dayIndex = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+    switch (dayIndex) {
+      case 0: {
+        this.startDay = 'first-day';
+        break;
+      }
+      case 1: {
+        this.startDay = 'second-day';
+        break;
+      }
+      case 2: {
+        this.startDay = 'third-day';
+        break;
+      }
+      case 3: {
+        this.startDay = 'fourth-day';
+        break;
+      }
+      case 4: {
+        this.startDay = 'fifth-day';
+        break;
+      }
+      case 5: {
+        this.startDay = 'sixth-day';
+        break;
+      }
+      case 6: {
+        this.startDay = 'seventh-day';
+        break;
+      }
+      default: {
+        this.startDay = 'first-day';
+        break;
+      }
+    }
   }
 
 }
